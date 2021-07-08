ffc3128d7df1e5fb3db006f20035d0fc59ff9ab0
Ji-Ha <xodhkd36@naver.com>
2021-07-08 17:31:07 +0900

    email + password request param

--- src/main/java/site/grigo/controller/AccountController.java
+++ src/main/java/site/grigo/controller/AccountController.java
@@ -1,7 +1,18 @@
 package site.grigo.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
+@RequestMapping("/")
 public class AccountController {
+
+    @PostMapping("/login")
+    public String signIn(String email, String password, HttpServletResponse response){
+        return "ok";
+    }
 }
--- src/main/java/site/grigo/domain/Account.java
+++ src/main/java/site/grigo/domain/Account.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Account {
-    private String ID;
+    private Long Id;
     private String name;
     private String phone;
     private Integer student_id;
