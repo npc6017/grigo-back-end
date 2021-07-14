bce6a13a379191e65f406d076f45ab3fd27738d7
Ji-Ha <xodhkd36@naver.com>
2021-07-14 18:43:18 +0900

    create(AccountTag) : AccountTag domain

--- src/main/java/site/grigo/domain/account/ProfileDTO.java
+++ src/main/java/site/grigo/domain/account/ProfileDTO.java
@@ -3,6 +3,11 @@ package site.grigo.domain.account;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import site.grigo.domain.accounttag.AccountTag;
+import site.grigo.domain.tag.Tag;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Account Update : Phone, Birth | ProfileDTO
@@ -16,6 +21,7 @@ public class ProfileDTO {
     private String phone;
     private String birth;
     private String sex;
+    private List<AccountTag> tags;
 
     public ProfileDTO() {
     }
