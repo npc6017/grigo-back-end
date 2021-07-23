4ffea2cdfa96effc4a85942d975535c246f59fc3
Ji-Ha <xodhkd36@naver.com>
2021-07-23 16:00:06 +0900

    refactor(commentDTO) : add id, timestamp

--- src/main/java/site/grigo/domain/comment/CommentDTO.java
+++ src/main/java/site/grigo/domain/comment/CommentDTO.java
@@ -3,8 +3,20 @@ package site.grigo.domain.comment;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter @Setter
 public class CommentDTO {
-
+    private Long id;
     private String content;
+    private LocalDateTime timeStamp;
+
+    public CommentDTO(Long id, String content, LocalDateTime timeStamp) {
+        this.id = id;
+        this.content = content;
+        this.timeStamp = timeStamp;
+    }
+    public CommentDTO() {
+
+    }
 }
